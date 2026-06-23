6e2c80c835355e36e4c05a90970135b7124049d4
SEO SEONGMIN <ssigssige9230@gmail.com>
2026-06-23 17:16:19 +0900

    chore(be): 백엔드 초기 환경 세팅
    - start.spring.io 를 통해 초기 환경 구성
    	- Build(Gradle-Groovy): 실행 모델이 task graph으로써 maven 보다 좀 더 유연하기 때문에 선택
    	- Spring Boot(4.1.0): GA 중 가장 최신 버전 선택
    	- Package Name(com.nidus.twinly): nidus 팀의 twinly 프로젝트
    	- Packaging(Jar): Spring Boot의 경우, Tomcat 서버를 함께 빌드하는 것이 설계 철학이므로 선택
    	- Configuration(YAML): 좀 더 구조화되어 읽기 편한 YAML 선택
    	- Java(21): LTS 중 생태계가 구축되어있고 참고 자료가 많은 21 선택

---
+++ backend/src/main/java/com/nidus/twinly/TwinlyApplication.java
@@ -0,0 +1,13 @@
+package com.nidus.twinly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwinlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwinlyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nidus/twinly/TwinlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nidus.twinly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwinlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
