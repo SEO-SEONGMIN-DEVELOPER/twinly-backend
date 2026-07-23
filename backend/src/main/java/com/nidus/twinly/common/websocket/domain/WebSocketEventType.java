d2b7f46c5bbc35c27a48ef52898ea9b8bbdc75ca
SEO SEONGMIN <ssigssige9230@gmail.com>
2026-07-23 12:52:58 +0900

    chore(be): 로그 디렉터리를 gitignore에 추가
    애플리케이션이 남기는 backend/logs가 추적 대상에 잡혀
    매 커밋마다 산출물이 섞이던 것을 제외.

--- backend/src/main/java/com/nidus/twinly/common/websocket/domain/WebSocketEventType.java
+++
@@ -1,8 +0,0 @@
-package com.nidus.twinly.common.websocket.domain;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum WebSocketEventType {
-    @JsonProperty("chatMsg") CHAT_MSG,
-    @JsonProperty("seasonChange") SEASON_CHANGE
-}
